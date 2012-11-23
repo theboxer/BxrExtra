81427b007fca879ae96010126d89fa227866dbf5
TheBoxer <theboxercz@gmail.com>
2012-11-23 17:07:40 +0100

    Added czech translation.

---
+++ core/components/bxrextra/lexicon/cs/default.inc.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Default Czech Lexicon Entries for BxrExtra
+ *
+ * @package bxrextra
+ * @subpackage lexicon
+ */
+$_lang['bxrextra'] = 'BxrExtra';
+$_lang['bxrextra.menu_desc'] = 'Ukázkové rozšíření určené pro další vývoj.';
+$_lang['bxrextra.items'] = 'Položky';
+$_lang['bxrextra.item_create'] = 'Přidat položku';
+$_lang['bxrextra.item_err_ae'] = 'Položka s tímto názvem již existuje.';
+$_lang['bxrextra.item_err_nf'] = 'Položka nenalezena.';
+$_lang['bxrextra.item_err_ns'] = 'Položka nebyla specifikována.';
+$_lang['bxrextra.item_err_remove'] = 'Nastal problém při odstraňování položky.';
+$_lang['bxrextra.item_err_save'] = 'Nastal problém při ukládání položky.';
+$_lang['bxrextra.item_remove'] = 'Odstranit položku';
+$_lang['bxrextra.item_remove_confirm'] = 'Opravdu chcete odstranit tuto položku?';
+$_lang['bxrextra.item_update'] = 'Upravit položku';
+$_lang['bxrextra.intro_msg'] = 'Spravovat položky.';
+$_lang['bxrextra.search...'] = 'Hledat...';
---
+++ core/components/bxrextra/lexicon/cs/properties.inc.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Properties Czech Lexicon Entries for BxrExtra
+ *
+ * @package bxrextra
+ * @subpackage lexicon
+ */
+$_lang['prop_bxrextra.limit_desc'] = 'Počet položek na stránku.';
+$_lang['prop_bxrextra.outputseparator_desc'] = 'Řetězec pro oddělení řádků.';
+$_lang['prop_bxrextra.sortby_desc'] = 'Pole, podle kterého se budou položky řadit.';
+$_lang['prop_bxrextra.sortdir_desc'] = 'Směr řazení.';
+$_lang['prop_bxrextra.tpl_desc'] = 'Chunk, který bude použit pro každý řádek s položkami.';
+$_lang['prop_bxrextra.toplaceholder_desc'] = 'Pokud je nastaveno, bude obsah uložen do placeholderu místo toho, aby byl přímo vypsán.';
