354423d3a754ad1ea71d7e80938573a05de86d13
TheBoxer <theboxercz@gmail.com>
2013-03-20 11:22:29 +0100

    Fixed updating item from grid and window. Removed config core and added sample.

--- core/components/bxrextra/processors/mgr/item/update.class.php
+++ core/components/bxrextra/processors/mgr/item/update.class.php
@@ -11,22 +11,15 @@ class BxrExtraUpdateProcessor extends modObjectUpdateProcessor {
     public $languageTopics = array('bxrextra:default');
     public $objectType = 'bxrextra.items';
 
-    public function beforeSave() {
-//        $name = $this->getProperty('name');
-//        $id = $this->getProperty('id');
-//
-//        /** @var BxrExtraItem $currentItem */
-//        $currentItem = $this->modx->getObject($this->classKey, $id);
-//
-//        if (empty($name)) {
-//            $this->addFieldError('name',$this->modx->lexicon('bxrextra.item_err_ns_name'));
-//
-//        } else if ($this->modx->getCount($this->classKey, array('name' => $name)) && $currentItem->get('name') != $name) {
-//            $this->addFieldError('name',$this->modx->lexicon('bxrextra.item_err_ae'));
-//        }
-//
-//        die(var_dump($this->getProperties()));
+    public function beforeSet() {
+        $name = $this->getProperty('name');
 
+        if (empty($name)) {
+            $this->addFieldError('name',$this->modx->lexicon('bxrextra.item_err_ns_name'));
+
+        } else if ($this->modx->getCount($this->classKey, array('name' => $name)) && ($this->object->name != $name)) {
+            $this->addFieldError('name',$this->modx->lexicon('bxrextra.item_err_ae'));
+        }
         return parent::beforeSave();
     }
 
